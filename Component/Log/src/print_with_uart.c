b03a421a1b07704d26d26b604e3f4089f05bd934
mammadabbasi <work.abbasii@gmail.com>
2024-03-25 00:17:22 +0330

    replace seven segment deriver

--- Component/Log/src/print_with_uart.c
+++ Component/Log/src/print_with_uart.c
@@ -1,6 +1,6 @@
 
 #include"main.h"
-
+#include"print_with_uart"
 
 
 void UART_Printf(const char *fmt, ...)
--- Component/seven_segment_and_led/inc/seven_segment_and_LEDS.h
+++ Component/seven_segment_and_led/inc/seven_segment_and_LEDS.h
@@ -1,14 +1,9 @@
 #ifndef _SEVEN_SEGMENT_AND_LED_DERIVER
 #define _SEVEN_SEGMENT_AND_LED_DERIVER
 #include "main.h"
-#include "tim.h"
-#include "adc.h"
-#include "usart.h"
 #include "stm32f1xx_hal_gpio.h"
-#include "string.h"
-#include "w25qxx.h"
 #include "Typedef.h"
-#include "write_to_flash.h"
+
 
 
 void SevenSegmentDisplay(uint8_t number);
