e88a064a4d2e13f5d8a637cb6f494977327bcb0a
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-07-22 20:02:46 +0330

    change naming

--- MySource/Component/Voice_recorder/inc/ReadKeyboard.h
+++ MySource/Component/Voice_recorder/inc/ReadKeyboard.h
@@ -8,6 +8,11 @@
 #define DEBOUNCE_DELAY 50 // Debounce delay in milliseconds
 #define PRESS_DELAY 5000  // Press delay in milliseconds (5 seconds)
 
+/**
+ * @brief Main keyboard handler.
+ * This function reads key presses, manages debounce, and updates the recorder state based on the pressed key.
+ * @return void
+ */
 void KeyBoard();
 
 #endif
