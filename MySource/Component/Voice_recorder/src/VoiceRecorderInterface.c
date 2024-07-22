87ddae7452c45fc6b71c0644bb9c06abb983620b
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-07-22 20:15:39 +0330

    write comment for voice recorder interface .c

--- MySource/Component/Voice_recorder/src/VoiceRecorderInterface.c
+++ MySource/Component/Voice_recorder/src/VoiceRecorderInterface.c
@@ -1,6 +1,11 @@
 #include "VoiceRecorderInterface.h"
 
-
+/**
+ * @brief Main function for voice recorder state management.
+ * This function handles the state machine for the voice recorder, managing various states such as initialization,
+ * reading keyboard input, recording, playing, stopping, switching tracks, and erasing flash.
+ * @return void
+ */
 void VoiceRecorder()
 {
   switch (VoiceRecorderSt.State)
@@ -55,15 +60,12 @@ void VoiceRecorder()
   }
   }
 }
-void NextTrack()
-{
-  VoiceRecorderSt.Track++;
-  if (VoiceRecorderSt.Track > (MaxNumberOfVoice - 1))
-  {
-    VoiceRecorderSt.Track = 1;
-  }
-  SevenSegmentDisplay(VoiceRecorderSt.Track);
-}
+
+/**
+ * @brief Restores information from flash memory.
+ * This function retrieves saved details from flash memory and updates the track and recorded array information.
+ * @return void
+ */
 void RestoreInformationFromFlash()
 {
   W25q_RestoreDetail(VoiceRecorderSt.Voice.RecordedArray, VoiceRecorderSt.Track, &(VoiceRecorderSt.Voice.CountOfSavedArray));
@@ -75,6 +77,12 @@ void RestoreInformationFromFlash()
     VoiceRecorderSt.Voice.CountOfSavedArray = 0;
   }
 }
+
+/**
+ * @brief Initializes the voice recorder.
+ * This function sets up the initial state of the voice recorder, including counters, flags, and hardware calibration.
+ * @return void
+ */
 void VoiceRecorderInitiation()
 {
   VoiceRecorderSt.ADC.Counter = 0;
@@ -96,6 +104,12 @@ void VoiceRecorderInitiation()
   Blinking();
   SevenSegmentDisplay(VoiceRecorderSt.Track);
 }
+
+/**
+ * @brief Restores the factory settings.
+ * This function erases the flash memory, reinitializes the voice recorder, and updates the LED indicators.
+ * @return void
+ */
 void RestFactoryFunction()
 {
   HAL_GPIO_WritePin(GPIOB, Record_LED_Pin, GPIO_PIN_SET);
@@ -106,6 +120,12 @@ void RestFactoryFunction()
   HAL_GPIO_WritePin(GPIOB, Record_LED_Pin, GPIO_PIN_RESET);
   HAL_GPIO_WritePin(GPIOB, Play_LED_Pin, GPIO_PIN_RESET);
 }
+
+/**
+ * @brief Interrupt service routine.
+ * This function handles the interrupt, deciding whether to sample ADC values or generate PWM waves based on the current mode.
+ * @return void
+ */
 void InterruptFunc()
 {
   if (VoiceRecorderSt.Flag.InterruptSwitch == 1)
@@ -116,4 +136,19 @@ void InterruptFunc()
   {
     MakePwmWave();
   }
+}
+
+/**
+ * @brief Switches to the next track.
+ * This function increments the current track and updates the display. It wraps around if the maximum number of tracks is exceeded.
+ * @return void
+ */
+void NextTrack()
+{
+  VoiceRecorderSt.Track++;
+  if (VoiceRecorderSt.Track > (MaxNumberOfVoice - 1))
+  {
+    VoiceRecorderSt.Track = 1;
+  }
+  SevenSegmentDisplay(VoiceRecorderSt.Track);
 }
